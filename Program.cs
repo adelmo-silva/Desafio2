57a73054c15cf9442b738d38f5a90175640d179d
Adelmo Silva <adelmotocomsono@gmail.com>
2021-11-22 00:58:22 -0300

    Única versão

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿{
+    Console.Write("Informe a primeira nota: ");
+    double n1 = double.Parse(Console.ReadLine());
+    Console.Write("Informe a segunda nota: ");
+    double n2 = double.Parse(Console.ReadLine());
+    Console.Write("informe a terceira nota: ");
+    double n3 = double.Parse(Console.ReadLine());
+    Console.Write("Informe a quarta nota: ");
+    double n4 = double.Parse(Console.ReadLine());
+    double média = (n1 + n2 + n3 + n4) / 4;
+    
+    if(média >= 0 && média <= 3)
+    {
+        Console.Write("Sua média é " + média + ", você foi reprovado!");
+    }
+    if(média >= 4 && média <= 6)
+    {
+        Console.Write("Sua média é " + média + ", você está de recuperação!");
+    }
+    if(média >= 7 && média <= 10)
+    {
+        Console.Write("Sua média é " + média + ", você foi aprovado!");
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,5 @@
+Ler 4 notas, calcular a média e em seguida deve mostrar uma das mensagens:
+
+De 0 a 4 (abaixo da média)
+De 5 a 7 (média ok)
+De 8 a 10 (excelente)
